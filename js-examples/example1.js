605aed5ec2520d107d8fd0056de6cb0158040d36
aditi01 <aditi.w4@gmail.com>
2015-07-05 19:09:58 +0530

    deleted test.html

---
+++ js-examples/example1.js
@@ -0,0 +1,251 @@
+var testArray = [];
+testArray= [{  
+            "post_date":"Jun 20",
+            "post_id":"post_01",
+            "display_name":"greg",
+            "location_name": "Mumbai",
+            "channel_name": "Arts",
+            "post_content":"xyz abc def",
+            "upvotes":1
+        },
+        {  
+            "post_date":"Jun 20",
+            "post_id":"post_02",
+            "display_name":"ash",
+            "location_name": "Mumbai",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":2
+        },
+        {  
+            "post_date":"Jun 19",
+            "post_id":"post_03",
+            "display_name":"adi",
+            "location_name": "Bangalore",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":3
+        },
+        {  
+            "post_date":"Jun 15",
+            "post_id":"post_04",
+            "display_name":"adi",
+            "location_name": "Bangalore",
+            "channel_name": "Arts",
+            "post_content":"xyz abc def",
+            "upvotes":4
+        },
+        {  
+            "post_date":"May 29",
+            "post_id":"post_05",
+            "display_name":"adi",
+            "location_name": "Delhi",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":3
+        },
+        {  
+            "post_date":"May 23",
+            "post_id":"post_06",
+            "display_name":"greg",
+            "location_name": "Mumbai",
+            "channel_name": "Science",
+            "post_content":"xyz abc def",
+            "upvotes":4
+        },
+        {  
+            "post_date":"May 21",
+            "post_id":"post__07",
+            "display_name":"greg",
+            "location_name": "Bangalore",
+            "channel_name": "Science",
+            "post_content":"xyz abc def",
+            "upvotes":3
+        },
+        {  
+            "post_date":"May 20",
+            "post_id":"post_08",
+            "display_name":"ash",
+            "location_name": "Delhi",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":1
+        },
+        {  
+            "post_date":"May 15",
+            "post_id":"post_09",
+            "display_name":"ash",
+            "location_name": "Bangalore",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":2
+        },
+        {  
+            "post_date":"May 10",
+            "post_id":"post_10",
+            "display_name":"ash",
+            "location_name": "Mumbai",
+            "channel_name": "Arts",
+            "post_content":"xyz abc def",
+            "upvotes":7
+        },
+        {  
+            "post_date":"May 01",
+            "post_id":"post_11",
+            "display_name":"adi",
+            "location_name": "Mumbai",
+            "channel_name": "Arts",
+            "post_content":"xyz abc def",
+            "upvotes":5
+        },
+        {  
+            "post_date":"Mar 19",
+            "post_id":"post_12",
+            "display_name":"adi",
+            "location_name": "Delhi",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":7
+        },
+        {  
+            "post_date":"Mar 18",
+            "post_id":"post_13",
+            "display_name":"greg",
+            "location_name": "Delhi",
+            "channel_name": "Science",
+            "post_content":"xyz abc def",
+            "upvotes":2
+        },
+        {  
+            "post_date":"Mar 12",
+            "post_id":"post_14",
+            "display_name":"adi",
+            "location_name": "Bangalore",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":2
+        },
+        {  
+            "post_date":"Feb 09",
+            "post_id":"post_15",
+            "display_name":"greg",
+            "location_name": "Bangalore",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":4
+        },
+        {  
+            "post_date":"Feb 08",
+            "post_id":"post_16",
+            "display_name":"greg",
+            "location_name": "Bangalore",
+            "channel_name": "Arts",
+            "post_content":"xyz abc def",
+            "upvotes":5
+        },
+        {  
+            "post_date":"Feb 07",
+            "post_id":"post_17",
+            "display_name":"ash",
+            "location_name": "Bangalore",
+            "channel_name": "Arts",
+            "post_content":"xyz abc def",
+            "upvotes":6
+        },
+        {  
+            "post_date":"Feb 06",
+            "post_id":"post_18",
+            "display_name":"greg",
+            "location_name": "Mumbai",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":1
+        },
+        {  
+            "post_date":"Feb 05",
+            "post_id":"post_19",
+            "display_name":"ash",
+            "location_name": "Delhi",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":4
+        },
+        {  
+            "post_date":"Jan 22",
+            "post_id":"post_20",
+            "display_name":"greg",
+            "location_name": "Mumbai",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":5
+        },
+        {  
+            "post_date":"Jan 21",
+            "post_id":"post_21",
+            "display_name":"ash",
+            "location_name": "Mumbai",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":2
+        },
+        {  
+            "post_date":"Jan 20",
+            "post_id":"post_22",
+            "display_name":"greg",
+            "location_name": "Delhi",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":4
+        },
+        {  
+            "post_date":"Jan 19",
+            "post_id":"post_23",
+            "display_name":"adi",
+            "location_name": "Delhi",
+            "channel_name": "Lifestyle",
+            "post_content":"xyz abc def",
+            "upvotes":4
+        },
+        {  
+            "post_date":"Jan 18",
+            "post_id":"post_24",
+            "display_name":"adi",
+            "location_name": "Mumbai",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":3
+        },
+        {  
+            "post_date":"Jan 01",
+            "post_id":"post_25",
+            "display_name":"ash",
+            "location_name": "Bangalore",
+            "channel_name": "Music",
+            "post_content":"xyz abc def",
+            "upvotes":3
+        }];
+
+console.log(testArray);
+var i;
+var max=0;
+for(i=0; i<testArray.length;i++){
+	if(max<=testArray[i].upvotes)
+		max=testArray[i].upvotes;
+}
+
+console.log(max);
+
+var min=testArray[0].upvotes;
+for(i=0; i<testArray.length;i++){
+	if(min>testArray[i].upvotes)
+		min=testArray[i].upvotes;
+}
+
+console.log(min);
+
+for(i=0; i<testArray.length;i++){
+	if(min==testArray[i].upvotes)
+		testArray[i].upvotes++;
+}
+
+console.log(testArray);
