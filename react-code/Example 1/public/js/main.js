db07ae6bb0c8d833d71daaeea6be63ae53594484
aditi01 <aditi.w4@gmail.com>
2015-12-02 07:44:01 +0530

    Todo App

--- react-code/Example 1/public/js/main.js
+++ react-code/Example 1/public/js/main.js
@@ -1,8 +1,60 @@
 var Todo = React.createClass({
 
+	getInitialState: function() {
+		return {editing: false}
+	},
+
+	edit: function() {
+		this.setState({editing: true});
+	},
+
+	todoDisplay: function() {
+			 return(
+				<li className = "todo"> 
+					<span onClick = {this.edit}>
+						{this.props.children}
+					</span>
+					<button className = "btn btn-default btn-sm glyphicon glyphicon-trash pull-right" />
+				</li>		
+		);
+	},
+	save: function() {
+		var val = this.refs.newValue.getDOMNode().value;
+		alert('Todo'+ val +' saved');
+		this.setState({editing: false});
+	},
+
+	todoForm: function() {
+			 return(
+				<li className = "todo"> 
+					<span>
+						<input type="text" placeholder = "Edit the Todo" ref = "newValue" defaultValue = {this.props.children} />
+					</span>
+					<button onClick={this.save} className = "btn btn-default btn-sm glyphicon glyphicon-floppy-disk pull-right" />
+				</li>		
+		);
+	},
 	render: function() {
-	 return(
-			<div> 
+		if(this.state.editing) {
+			return this.todoForm();
+		}
+		else {
+			return this.todoDisplay();
+		}
+	},
+});
+
+
+var TodoList = React.createClass({
+	getInitialState: function() {
+		return{
+			todos: ['Hello Aditi!',
+					'Hello Ashwin!']
+		}
+	},
+
+	render: function() {
+			return (<div> 
 				<h1> Things To Do </h1>
 				<div className = "form-inline">
 					<div className = "form-group">
@@ -10,14 +62,15 @@ var Todo = React.createClass({
 						<button className = "btn btn-sm btn-default">+</button>
 					</div>
 				</div>
+			<ul>
+				{this.state.todos.map(function (todo) {
+					return (<Todo> {todo} </Todo>);
+				})
+			}
+			</ul>
 
-				<ul>
-					<li className = "todo"> Hello Aditi </li>
-					<li className = "todo"> Find a job </li>
-				</ul>
-			</div>
-		);
-	},
+			</div>);
+		}
 });
 
-React.render(<Todo/>, document.getElementById('todo'));
+React.render(<TodoList />, document.getElementById('todo'));
